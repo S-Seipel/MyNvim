b2d83f60a1d18b07e5d75db4f722410b29940e1f
S-Seipel <santiagobernabeseipel@gmail.com>
2025-11-02 23:06:30 -0300

    agrego el 'alt + j/k' para mover lienas de codigo, y el Tab para identar en modo normal

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 require("config.lazy")
 require('config/options')
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<Tab>", ">gv", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-Tab>", "<gv", { noremap = true, silent = true })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,42 @@
-return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		vim.lsp.enable({"ts_ls"})
-	end
-}
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    vim.lsp.enable({
+      "jsonls",        -- json-lsp
+      "cssls",         -- css-lsp
+      "html",          -- html-lsp
+      "yamlls",        -- yaml-language-server
+      "intelephense",  -- intelephense
+      "emmet_ls",      -- emmet-ls
+      "ts_ls",         -- typescript-language-server
+			"tailwindcss",
+    })
+
+    local ok, lspconfig = pcall(require, "lspconfig")
+    if ok then
+      lspconfig.ts_ls.setup({
+        settings = {
+          typescript = {
+            preferences = { importModuleSpecifier = "relative" },
+            tsserver = { useSyntaxServer = "auto" },
+          },
+          javascript = {
+            preferences = { importModuleSpecifier = "relative" },
+          },
+        },
+        root_dir = lspconfig.util.root_pattern(
+          "tsconfig.json",
+          "package.json",
+          ".git"
+        ),
+      })
+    end
+
+    vim.diagnostic.config({
+      virtual_text = { prefix = "●" },
+      signs = true,
+      underline = true,
+      severity_sort = true,
+    })
+  end,
+}
